56d84ecb37159352788e529ec8ef75c17a4876b4
AmrAbouElkair <amrmahmooud20022002@gmail.com>
2024-02-13 19:29:41 +0200

    Project and packages initialization

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+// Fontawesome
+import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
